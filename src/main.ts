8e55d706f404723a037d22c6557a1eba6afb7806
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-01-23 14:47:35 +0100

    Add v3-scroll-lock package and import V3ScrollLock in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import routes from '~pages'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import V3ScrollLock from 'v3-scroll-lock';
 const app = createApp(App)
+app.use(V3ScrollLock, {})
 app.use(
   createRouter({
     history: createWebHistory(),
