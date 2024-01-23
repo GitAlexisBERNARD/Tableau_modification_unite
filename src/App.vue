b7e4c383fcdd9b07d227097954c125935ccfc938
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-01-23 14:39:02 +0100

    Initial init no unplugin vue router

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Suspense } from 'vue'
+import HeaderPage from '@/components/HeaderPage.vue'
+import FooterPage from '@/components/FooterPage.vue'
+</script>
+
+<template>
+    <HeaderPage />
+    <main>
+        <Suspense>
+            <RouterView />
+        </Suspense>
+    </main>
+    <FooterPage />
+</template>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+    <header>
+        <h1>My App</h1>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/About">About</RouterLink>
+            <RouterLink to="/aboutest">About2</RouterLink>
+        </nav>
+    </header>
+</template>
